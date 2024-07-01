bb3d529c7a5a04fe36c752efcc25ba6caccb5922
Eugene <eugenewwj@gmail.com>
2024-07-02 01:11:21 +0800

    remove un use styles

--- screens/FOMOPayScreen.js
+++ screens/FOMOPayScreen.js
@@ -1,6 +1,6 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {WebView} from 'react-native-webview';
-import {Linking, StyleSheet, View} from 'react-native';
+import {Linking, View} from 'react-native';
 import {GlobalContext} from '../states/GlobalState';
 import {CN, RESET} from '../constants/Constant';
 import calculate from '../services/DimensionAdapter';
@@ -68,49 +68,3 @@ export default function FOMOPayScreen({route, navigation}) {
     </TimerLayout>
   );
 }
-
-const styles = StyleSheet.create({
-  image: {
-    flexDirection: 'row',
-    width: '100%',
-    height: calculate(50),
-  },
-  timerPosition: {
-    marginTop: calculate(10),
-    position: 'absolute',
-    right: calculate(5),
-  },
-  timer: {
-    width: calculate(70),
-    height: calculate(30),
-    resizeMode: 'stretch',
-  },
-  timerText: {
-    textAlign: 'center',
-    paddingTop: calculate(7),
-    fontWeight: 'bold',
-    fontSize: calculate(20),
-    color: 'white',
-  },
-  homePosition: {
-    position: 'absolute',
-    right: '42%',
-    bottom: calculate(120),
-  },
-  homeBtn: {
-    width: calculate(50),
-    height: calculate(50),
-    resizeMode: 'stretch',
-  },
-  backBtn: {
-    margin: calculate(15),
-    width: calculate(24),
-    height: calculate(20),
-  },
-  purchaseTitle: {
-    width: calculate(180),
-    height: calculate(40),
-    margin: calculate(4),
-    marginLeft: calculate(55),
-  },
-});
